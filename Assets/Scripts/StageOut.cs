a4dc5f69fc7dd87ab1b5803dd5b326144a0cb3e6
yato-0914 <yato.school0914@gmail.com>
2026-06-18 18:50:58 +0900

    a

--- Assets/Scripts/StageOut.cs
+++ Assets/Scripts/StageOut.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 public class StageOut : MonoBehaviour
 {
     //アクセス修飾子 型と変数
-    private int Score;
+    private int Score = 0;
+
+    public TextMeshProUGUI scoreText;
     //このコードをアタッチしたオブジェクトに他のオブジェクトがすり抜けたに呼ばれる
     void OnTriggerEnter(Collider other )
     {
         Score = Score + 1;
-        Debug.Log($"スコア:{Score}");
+        scoreText.text = $"スコア:{Score}";
+
 
         //このコードをアタッチしたオブジェクトに他のオブジェクトがすり抜けたに呼ばれる
         //ebug.Log($"{other.name}がすりぬけた");
         // すり抜けたオブジェクトを破棄
+        Debug.Log($":{other.name}がすり抜けました。");
+
         Destroy(other.gameObject);
 
     }
